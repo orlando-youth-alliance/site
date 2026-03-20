19d6e9e11cc2bf11d7936a561103267adc69974e
hectorsector <hector@hectorsector.com>
2026-03-20 12:06:54 -0400

    handle race condition on actions

--- cms/pb_hooks/on_content_change.pb.js
+++ cms/pb_hooks/on_content_change.pb.js
@@ -1,15 +1,35 @@
-onRecordAfterCreateSuccess((e) => {
-  $app.logger().info("onRecordAfterCreateSuccess fired");
+function dispatchIfIdle() {
+  const token = $os.getenv("GITHUB_TOKEN")
+  const runsUrl = "https://api.github.com/repos/orlando-youth-alliance/site/actions/workflows/hugo.yml/runs"
+  const headers = {
+    Authorization: "Bearer " + token,
+    Accept: "application/vnd.github+json",
+  }
+
+  for (const status of ["queued", "in_progress"]) {
+    const res = $http.send({
+      method: "GET",
+      url: runsUrl + "?status=" + status + "&per_page=1",
+      headers,
+    })
+    const data = JSON.parse(res.raw)
+    if (data.total_count > 0) {
+      $app.logger().info("deploy already pending, skipping dispatch (status=" + status + ")")
+      return
+    }
+  }
+
   $http.send({
     method: "POST",
-    url: "https://api.github.com/repos/orlando-youth-alliance/site/actions/workflows/hugo.yml/dispatches",
-    headers: {
-      Authorization: "Bearer " + $os.getenv("GITHUB_TOKEN"),
-      Accept: "application/vnd.github+json",
-      "Content-Type": "application/json",
-    },
+    url: runsUrl.replace("/runs", "/dispatches"),
+    headers: { ...headers, "Content-Type": "application/json" },
     body: JSON.stringify({ ref: "main" }),
-  });
+  })
+}
+
+onRecordAfterCreateSuccess((e) => {
+  $app.logger().info("onRecordAfterCreateSuccess fired");
+  dispatchIfIdle();
   e.next();
 });
 
@@ -19,30 +39,12 @@ onRecordAfterUpdateSuccess((e) => {
     return;
   }
   $app.logger().info("onRecordAfterUpdateSuccess fired");
-  $http.send({
-    method: "POST",
-    url: "https://api.github.com/repos/orlando-youth-alliance/site/actions/workflows/hugo.yml/dispatches",
-    headers: {
-      Authorization: "Bearer " + $os.getenv("GITHUB_TOKEN"),
-      Accept: "application/vnd.github+json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ ref: "main" }),
-  });
+  dispatchIfIdle();
   e.next();
 });
 
 onRecordAfterDeleteSuccess((e) => {
   $app.logger().info("onRecordAfterDeleteSuccess fired");
-  $http.send({
-    method: "POST",
-    url: "https://api.github.com/repos/orlando-youth-alliance/site/actions/workflows/hugo.yml/dispatches",
-    headers: {
-      Authorization: "Bearer " + $os.getenv("GITHUB_TOKEN"),
-      Accept: "application/vnd.github+json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ ref: "main" }),
-  });
+  dispatchIfIdle();
   e.next();
 });
