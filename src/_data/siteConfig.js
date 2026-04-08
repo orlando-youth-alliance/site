fee21a4b337d31bfd1b4dd81be011ad27697db1d
hectorsector <hector@hectorsector.com>
2026-04-08 11:56:52 -0400

    remove prior deps, cleanup

--- src/_data/siteConfig.js
+++
@@ -1,6 +0,0 @@
-import { fetchCollection } from "./lib/pocketbase.js";
-
-export default async function () {
-  const results = await fetchCollection("site_config", "?perPage=1");
-  return results[0] ?? {};
-}
